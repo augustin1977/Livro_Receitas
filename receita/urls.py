b401b4207cde42dddaa63d857d191920f22a57a7
Eric_Augustin <ericaugustin@hotmail.com>
2023-01-16 11:56:08 -0300

    Criando views

--- receita/urls.py
+++ receita/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path("cadastrar/",views.cadastrar)
+    path("cadastrar/",views.cadastrar),
+    path("cadastrar_receita/", views.cadastrar_receita),
+    path("cadastrar_unidade/", views.cadastrar_unidade),
+    path("cadastrar_material/", views.cadastrar_material),
+    path("cadastrar_ingrediente/", views.cadastrar_ingrediente),
+    
 ]
--- receita/views.py
+++ receita/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+def cadastrar_receita(requeste):
+    return HttpResponse("Cadastro Receita")
+def cadastrar_unidade(requeste):
+    return HttpResponse("Cadastro Unidade")
+def cadastrar_material(requeste):
+    return HttpResponse("Cadastro Material")
+def cadastrar_Ingrediente(requeste):
+    return HttpResponse("Cadastro Ingrediente")
+
+
 def cadastrar(request):
     return HttpResponse("Olá")
