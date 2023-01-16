487a608a5a5a84c0b9e7577153a48c21a5f4368a
Eric_Augustin <ericaugustin@hotmail.com>
2023-01-16 11:52:42 -0300

    Comentários adicionados no arquivo models.py

--- receita/models.py
+++ receita/models.py
@@ -1,26 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+"""Classe unidades de medida, cadastrar a unidade de medida e seu simbolo ou abreveação"""
 class Unidades(models.Model):
     simbolo=models.CharField( max_length=10)
     unidades=models.CharField(max_length=10)
     def __str__(self):
         return f"{self.unidades}"
-
+"""Classe Materis cria o cadastro de materiais usados nas receitas"""
 class Materiais(models.Model):
     nome=models.CharField( max_length=150)
     unidade=models.ForeignKey(Unidades, on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.nome} em {self.unidade}"
 
-
+""" Classe Ingredientes cria o cadastros de ingredientes usados em cada receita, ou seja é identico ao 
+cadastro de materiais, no entanto tem a vinculação com a receita"""
 class Ingediente(models.Model):
     nome=models.ForeignKey(Materiais, on_delete=models.CASCADE)
     quantidade=models.DecimalField( max_digits=12, decimal_places=2)
     def __str__(self):
         return f"{self.nome}"
 
-
+"""Classe receita é onde ficam aramzenadas as receitas propriamente ditas"""
 class Receita(models.Model):
     nome=models.CharField(max_length=150)
     ingredientes=models.ManyToManyField(Ingediente)
@@ -29,8 +30,7 @@ class Receita(models.Model):
     usuario=models.ForeignKey(User,on_delete=models.PROTECT)
     def __str__(self):
         return f"{self.nome} do {self.usuario} na data {self.data_cadastro}"
-# comentário so para criar novo branch testando
-# novo comentário
+
 
    
     
