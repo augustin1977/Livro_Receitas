512e52101a1990519508a96b50a128dee89dbc1a
Eric_Augustin <ericaugustin@hotmail.com>
2023-01-16 12:13:02 -0300

    Criando app de usuarios parte 1 - corrigindo bugs

--- livroReceitas/urls.py
+++ livroReceitas/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,usuarios
+from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("receita/",include("receita.urls")),
-    path("auth/", include(usuarios.url)),
+    path("auth/", include("usuarios.url")),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def login(requeste):
     return HttpResponse("Login de Usuário")
 
-def cadastro(requeste):
+def cadastrar(requeste):
     return HttpResponse("Cadastro de Usuário")
 
 def editar(requeste):
